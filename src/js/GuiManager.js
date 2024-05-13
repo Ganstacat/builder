c496e98b4e4d477b3b2152722b7f468c8dcc0198
= <=>
2024-05-13 23:06:42 +0500

    translated ui

--- src/js/GuiManager.js
+++ src/js/GuiManager.js
@@ -18,52 +18,58 @@ export class GuiManager {
 	}
 	setupOptions(){
 		let options = {
-			scalex:1,
-			scaley:1,
-			scalez:1,
+			"длина":1,
+			"высота":1,
+			"ширина":1,
 			
-			rotationx:0,
-			rotationy:0,
-			rotationz:0,
+			"поворотX":0,
+			"поворотY":0,
+			"поворотZ":0,
 			
-			color: 0xFFFFFF
+			цвет: 0xFFFFFF
 
 		};
 		this.options = options;
 		
 		let self = this;
 	
-		this.gui.add(options, 'scalex',0.1, 10).listen().onChange((e)=>{self.stage.setScale(self.stage.selectedObject, options.scalex, options.scaley, options.scalez );});
-		this.gui.add(options, 'scaley',0.1, 10).listen().onChange((e)=>{self.stage.setScale(self.stage.selectedObject, options.scalex, options.scaley, options.scalez );});
-		this.gui.add(options, 'scalez',0.1, 10).listen().onChange((e)=>{self.stage.setScale(self.stage.selectedObject, options.scalex, options.scaley, options.scalez );});
-		this.gui.add(options, 'rotationx',0, Math.PI, Math.PI/16).listen().onChange((e)=>{
-			self.stage.setRotation(self.stage.selectedObject, options.rotationx, options.rotationy, options.rotationz);
+		this.gui.add(options, 'длина',0.1, 10).listen().onChange((e)=>{
+			self.stage.setScale(self.stage.selectedObject, options["длина"], options["высота"], options["ширина"] );
 		});
-		this.gui.add(options, 'rotationy',0, Math.PI, Math.PI/16).listen().onChange((e)=>{
-			self.stage.setRotation(self.stage.selectedObject, options.rotationx, options.rotationy, options.rotationz);
+		this.gui.add(options, 'ширина',0.1, 10).listen().onChange((e)=>{
+			self.stage.setScale(self.stage.selectedObject, options["длина"], options["высота"], options["ширина"] );
 		});
-		this.gui.add(options, 'rotationz',0, Math.PI, Math.PI/16).listen().onChange((e)=>{
-			self.stage.setRotation(self.stage.selectedObject, options.rotationx, options.rotationy, options.rotationz);
+		this.gui.add(options, 'высота',0.1, 10).listen().onChange((e)=>{
+			self.stage.setScale(self.stage.selectedObject, options["длина"], options["высота"], options["ширина"] );
 		});
-		this.gui.addColor(options, 'color').onChange((e)=>{
+		this.gui.add(options, 'поворотX',0, Math.PI, Math.PI/16).listen().onChange((e)=>{
+			self.stage.setRotation(self.stage.selectedObject, options["поворотX"], options["поворотY"], options["поворотZ"]);
+		});
+		this.gui.add(options, 'поворотY',0, Math.PI, Math.PI/16).listen().onChange((e)=>{
+			self.stage.setRotation(self.stage.selectedObject, options["поворотX"], options["поворотY"], options["поворотZ"]);
+		});
+		this.gui.add(options, 'поворотZ',0, Math.PI, Math.PI/16).listen().onChange((e)=>{
+			self.stage.setRotation(self.stage.selectedObject, options["поворотX"], options["поворотY"], options["поворотZ"]);
+		});
+		this.gui.addColor(options, 'цвет').onChange((e)=>{
 			self.stage.setMeshColor(self.stage.selectedObject, e);
 		});
 		
 		this.listeners.push(()=>{
 			if(!self.stage.selectedObject) return;
 			
-			self.options.scalex = self.stage.selectedObject.scale.x;
-			self.options.scaley = self.stage.selectedObject.scale.y;
-			self.options.scalez = self.stage.selectedObject.scale.z;
+			self.options["длина"] = self.stage.selectedObject.scale.x;
+			self.options["высота"] = self.stage.selectedObject.scale.y;
+			self.options["ширина"] = self.stage.selectedObject.scale.z;
 			
-			self.options.rotationx = self.stage.selectedObject.rotation.x;
-			self.options.rotationy = self.stage.selectedObject.rotation.y;
-			self.options.rotationz = self.stage.selectedObject.rotation.z;
+			self.options["поворотX"] = self.stage.selectedObject.rotation.x;
+			self.options["поворотY"] = self.stage.selectedObject.rotation.y;
+			self.options["поворотZ"] = self.stage.selectedObject.rotation.z;
 			
-			if(self.stage.selectedObject.isMesh) self.options.color = self.stage.selectedObject.material.color.getHex();
+			if(self.stage.selectedObject.isMesh) self.options["цвет"] = self.stage.selectedObject.material.color.getHex();
 			else {
 				self.stage.applyToMeshes(self.stage.selectedObject,
-					(o)=>{self.options.color = o.material.color.getHex();}
+					(o)=>{self.options["цвет"] = o.material.color.getHex();}
 				)
 			}
 		})
