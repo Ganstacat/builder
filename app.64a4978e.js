b3427eace0b23e0243d383e253c6050e9dffc73a
= <=>
2024-06-05 09:46:29 +0500

    interface fix

--- app.64a4978e.js
+++ app.64a4978e.js
@@ -50245,10 +50245,11 @@ function addListeners(controller) {
     });
     const addElementSelector = document.querySelector("#addElement");
     addElementSelector.onchange = (e)=>{
-        addElementHandler(addElementSelector.value, controller);
+        elementAdderHandler(addElementSelector.value, controller);
+        addElementSelector.value = "reset";
     };
 }
-function addElementHandler(element, controller) {
+function elementAdderHandler(element, controller) {
     if (!element || element === "reset") return;
     let geometry;
     let material = new _three.MeshStandardMaterial();
